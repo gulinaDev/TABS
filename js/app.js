0628a19f8f76ce405c24d19b58e403b881677ee1
gulinaDev <kanybekovagulina0101@gmail.com>
2023-04-23 15:52:25 +0600

    second commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+// DOM - document object modal - обьектная модель документа
+// BOM - browser object modal - обьектная модель браузера
+
+
+// let age = prompt("How old are you?")
+// console.log(age)
+// alert(true)
+// confirm(true)
+
+// document -> html кектердин баары документ болуп саналат
+// const h1 = document.getElementsByTagName("h1")
+// const div =document.getElementsByClassName("box")
+// const list = document.getElementById("list")
+// console.log(h1)
+// console.log(div)
+// console.log(list)
+
+
+// const box = document.querySelector(".box")
+// const lists = document.querySelectorAll("ul.li")
+// const list = document.querySelector("#list")
+// console.log(list)
+// console.log(lists)
+// console.log(box)
+
+
+// const title = document.querySelector("h1")
+// title.style.color = "red"
+// title.style.background ="yellow"
+// title.style.fontSize ="3rem"
+// title.style.width = "200px"
+// console.log(title)
+
+
+// lists.forEach((el,idx) =>{
+//     if (idx === 1){
+//         el.style.color = "green"
+//     }else{
+//         el.style.color = "red"
+//     }
+// })
+
+
+// console.log(list.firstElementChild.style.color = "red")
+// console.log(list.lastElementChild.style.color = "green")
+// console.log(list)
+
+
+// Array.from(list.children).forEach(li =>{
+//     li.style.backgroundColor = "blue"
+// })
+
+
+// console.log(list.nextElementSibling)
+// console.log(list.previousSibling)
+// console.log(list.parentNode.parentNode)
+
+
+// const newTag = document.createElement("div")
+// newTag.innerHTML = "JAVASCRIPT"
+// box.append(newTag)
+// console.log(newTag)
+
+
+// const circle = document.createElement("div")
+// circle.style.width = "300px"
+// circle.style.height = "300px"
+// circle.style.background = "pink"
+// circle.style.borderRadius = "50%"
+// circle.style.animation = "anim 2s infinite"
+// box.append(circle)
+
+
